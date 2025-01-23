f21a3d148145de981d95a25e76cff4ee4efe9aab
ivanvmoreno <contacto@ivanmoreno.me>
2025-01-23 21:52:20 +0000

    chore: fix dataset download path

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,3 @@
 #bin/bash
-huggingface-cli download Magpie-Align/Magpie-Reasoning-V2-250K-CoT-Llama3 --local-dir ../datasets/magpie_v2_llama3 --repo-type dataset
-huggingface-cli download facebook/seamless-m4t-v2-large --local-dir ../models/seamless_m4t_v2_large  
+huggingface-cli download Magpie-Align/Magpie-Reasoning-V2-250K-CoT-Llama3 --local-dir ../datasets/raw/magpie_v2_llama3 --repo-type dataset
+huggingface-cli download facebook/seamless-m4t-v2-large --local-dir ../models/seamless_m4t_v2_large
